0cbd5b19013cd293449d8f0f7faaea4dc8ad0e00
Pooja Deshpande <pooja.is.deshpande@gmail.com>
2021-01-06 13:44:19 +0530

    Adding ios sdk methods in progress - iteration 1

--- ios/ReactNativeFlyy.m
+++ ios/ReactNativeFlyy.m
@@ -1,6 +1,7 @@
 #import "ReactNativeFlyy.h"
 #import "FlyyFramework/FlyyFramework.h"
 
+
 @implementation ReactNativeFlyy
 
 // To export a module named ReactNativeFlyy
@@ -9,14 +10,37 @@ RCT_EXPORT_MODULE(Flyy)
 static NSString * STAGE = @"STAGE";
 static NSString * PRODUCTION = @"PRODUCTION";
 
-
-
 RCT_EXPORT_METHOD(initSDK: (NSString *)partnerToken :(int)environment)
 {
     //Instance of flyy class
     Flyy *flyyInstance = [[Flyy alloc] init];
     //Initialize sdk
     [flyyInstance initSDKWithPartnerToken:partnerToken environment: environment];
+}
+
+RCT_EXPORT_METHOD(setUser: (NSString *)userId)
+{
+    //Instance of flyy class
+    Flyy *flyyInstance = [[Flyy alloc] init];
+    //Initialize sdk
+    [flyyInstance setUserWithExternalUserId:userId];
+}
+
+RCT_EXPORT_METHOD(setUserName: (NSString *)userName)
+{
+    //Instance of flyy class
+    Flyy *flyyInstance = [[Flyy alloc] init];
+    //Initialize sdk
+    [flyyInstance setUserNameWithUserName:userName];
+}
+
+RCT_EXPORT_METHOD(openOffersScreen)
+{
+    //Instance of flyy class
+//    FlyyHelper *flyyHelper = [[FlyyHelper alloc] init];
+    
+    //Initialize sdk
+//    [flyyInstance openOffersPageWithNavigationController:];
 }
 
 - (NSDictionary *)constantsToExport
